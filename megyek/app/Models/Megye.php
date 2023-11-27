4ebb68525c0686609ad952cdc73cad5cd9d3dbed
h162169 <zsombinagy02@gmail.com>
2023-11-27 16:33:37 +0100

    Alap

---
+++ megyek/app/Models/Megye.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Megye extends Model
+{
+    use HasFactory;
+}
